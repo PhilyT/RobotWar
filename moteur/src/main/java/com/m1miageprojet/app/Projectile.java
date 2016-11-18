70b99eab36212ee3a40dd1fa0a4bdfa7c9d0afbd
Nicolas <nicolas.kircun@gmail.com>
2016-11-18 17:35:54 +0100

    #9 : Avancement plugin attaque

--- moteur/src/main/java/com/m1miageprojet/app/Projectile.java
+++ moteur/src/main/java/com/m1miageprojet/app/Projectile.java
@@ -7,8 +7,6 @@
  */
 package com.m1miageprojet.app;
 
-import java.awt.Graphics;
-
 import com.m1miageprojet.interfacesplugins.IProjectile;
 import com.m1miageprojet.interfacesplugins.IRobot;
 
@@ -17,24 +15,32 @@ public class Projectile implements IProjectile {
 	private double x1, y1;
 	private double y2;
 	private double x2;
+	private double coeffDir;
 	
 	public Projectile(int xDep, int yDep, IRobot adversaire) {
 		this.x1 = xDep;
 		this.y1 = yDep;
 		
-		double coeffDir = ((adversaire.getY() - this.y1) / (adversaire.getX() - this.x1));
-//		System.out.println((adversaire.getY() - this.y1) + " // " + (adversaire.getX() - this.x1));
-//		System.out.println((adversaire.getY() - this.y1) / (adversaire.getX() - this.x1));
-//		this.x2 = (int) ((int) (xDep + 20) * coeffDir);
-		this.x2 = xDep + 20;
-		this.y2 = (yDep + 20) * coeffDir;
+		if(adversaire.getY() > this.y1 && adversaire.getX() > this.x1) {
+			coeffDir = ((adversaire.getY() - this.y1) / (adversaire.getX() - this.x1));
+		} else {
+			coeffDir = ((this.y1 - adversaire.getY()) / (this.x1 - adversaire.getX()));
+		}
+		
+		double milieuX = (this.x1 + adversaire.getX()) / 2;
+		double milieuY = (this.y1 + adversaire.getY()) / 2;
+
+		this.x2 = milieuX - 30;
+		this.y2 = (milieuY - 30) * coeffDir;
 	}
 
 	public void deplace() {
-		this.x1 += 30;
-		this.y1 += 30;
-		this.x2 += 30;
-		this.y2 += 30;
+		double rapportX = this.x1 / this.x2;
+		double rapportY = this.y1 / this.y2;
+		this.x1 += 30 * rapportX;
+		this.y1 += 30 * rapportY;
+		this.x2 += 30 * rapportX;
+		this.y2 += 30 * rapportY;
 	}
 
 	public double getX1() {
