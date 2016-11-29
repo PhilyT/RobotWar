2172148684e6df790c89e6741c1a6f85fd97f0f4
Tom Phily <tom.phily@live.fr>
2016-11-29 23:18:26 +0100

    #21 Deselection de plugins graphismes en cours de parties

--- moteur/src/main/java/com/m1miageprojet/gestionplugins/actionsswing/ActionGraphismes.java
+++ moteur/src/main/java/com/m1miageprojet/gestionplugins/actionsswing/ActionGraphismes.java
@@ -10,20 +10,28 @@ public class ActionGraphismes extends AbstractAction {
 
 	private String nomplugins;
 	private SwingRepository swing;
+	private boolean selected;
 	
 	public ActionGraphismes(String name, SwingRepository swing)
 	{
 		super(name);
 		nomplugins = name;
 		this.swing = swing;
+		selected = swing.getNomPluginsGraphismesSelectionne().contains(nomplugins);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(!swing.getNomPluginsGraphismesSelectionne().contains(nomplugins))
+		if(!selected)
     	{
 			swing.getNomPluginsGraphismesSelectionne().add(nomplugins);
+			selected = true;
     	}
+		else
+		{
+			swing.getNomPluginsGraphismesSelectionne().remove(nomplugins);
+			selected = false;
+		}
         swing.chargement(swing.getRepo());
         swing.getOutils().encodeToFile(swing.getApp().getRobots(), "Sauvegarde.xml");
 
